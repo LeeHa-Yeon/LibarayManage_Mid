8a925d989ea30f849af90d8012cc4b386ac626ec
Lee Ha Yeon <gkdus1613@icloud.com>
2021-04-27 23:57:27 +0900

    Initial commit

---
+++ src/main/java/middleTermProject/LibraryManagement.java
@@ -0,0 +1,15 @@
+package middleTermProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagement {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagement.class, args);
+        System.out.println();
+
+    }
+
+}
