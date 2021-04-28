155695ce1a383219c7bcf82b3dd0d3d3ebac879e
Lee Ha Yeon <gkdus1613@icloud.com>
2021-04-29 03:14:45 +0900

    id/pwd 찾기

--- src/main/java/middleTermProject/TestManager.java
+++ src/main/java/middleTermProject/TestManager.java
@@ -30,7 +30,18 @@ public class TestManager {
                 register();
                 break;
             case 3 :
-                System.out.println("아이디 / 비번 찾기");
+                System.out.println("\n---------- 아이디 비번 찾기 -----------\n");
+                System.out.print("1. 아이디 / 2.비번 찾기 : ");
+                int select2 = sc.nextInt();
+                if(select2==1){
+                    System.out.println("\n--------- 아이디 찾기 --------------");
+                    find_id();
+                    break;
+                }else {
+                    System.out.println("\n-----------비밀번호 찾기------------");
+                    find_pwd();
+                    break;
+                }
             default:
                 System.out.println("종료");
                 break;
@@ -127,5 +138,78 @@ public class TestManager {
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    // 아이디,비번 찾기
+    public void find_id(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n전화번호를 입력하세요 :");
+        String userInput = sc.next();
+
+        try {
+            String usersPwd = "/Users/hayeon/IdeaProjects/LibarayManage_Mid/UserInfo/allUserInfo.txt";
+            BufferedReader users = new BufferedReader(new FileReader(usersPwd));
+            String userList = users.readLine();
+            List<String> idList = new ArrayList<String>();
+            List<String> phoneList = new ArrayList<String>();
+
+            while ((userList = users.readLine()) != null) {
+                String[] userSplit = userList.split("/");
+                idList.add(userSplit[0]);
+                phoneList.add(userSplit[3]);
+            }
+            if (phoneList.contains(userInput)) {
+                // 만약 입력한 값이 있다면
+                int index = phoneList.indexOf(userInput);
+                System.out.print("\n---------> 찾는 아이디 : ");
+                System.out.println(idList.get(index));
+                System.out.println("");
+            }
+            else{
+                // 없다면
+                System.out.println("\n------> 찾는 아이디가 없습니다.\n");
+            }
+            mainMemu();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void find_pwd(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\n아이디를 입력하세요 :");
+        String userInput = sc.next();
+
+        try {
+            String usersPwd = "/Users/hayeon/IdeaProjects/LibarayManage_Mid/UserInfo/allUserInfo.txt";
+            BufferedReader users = new BufferedReader(new FileReader(usersPwd));
+            String userList = users.readLine();
+            List<String> idList = new ArrayList<String>();
+            List<String> pwdList = new ArrayList<String>();
+
+            while ((userList = users.readLine()) != null) {
+                String[] userSplit = userList.split("/");
+                idList.add(userSplit[0]);
+                pwdList.add(userSplit[1]);
+            }
+            if (idList.contains(userInput)) {
+                // 만약 입력한 값이 있다면
+                int index = idList.indexOf(userInput);
+                System.out.print("\n---------> 찾는 비밀번호 : ");
+                System.out.println(pwdList.get(index));
+                System.out.println("");
+            }
+            else{
+                // 없다면
+                System.out.println("\n------> 존재하지 않는 아이디입니다.\n");
+            }
+            mainMemu();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
